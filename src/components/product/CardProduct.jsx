d653bc45f7cc477190dd33d5af2d3c6e4eac04c9
juanangelaalma <juanlah77@gmail.com>
2022-04-24 03:32:13 +0700

    fetch product by id

--- src/components/product/CardProduct.jsx
+++ src/components/product/CardProduct.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const CardProduct = ({ id, image, title, price }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    return navigate(`/products/${id}`)
+  }
+
   return (
-    <div className="catalog__products-product">
+    <div onClick={handleClick} className="catalog__products-product">
       <div className='catalog__products-product_image'>
         <img src={image} alt="" />
       </div>
--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,38 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import productsApi from "../../apis/productsApi";
 
 import "./product.css";
 
 const Product = () => {
+  const [dataProduct, setDataProduct] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [count, setCount] = useState(1);
+  const params = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    productsApi
+      .get(`/${params.idProduct}`)
+      .then((response) => {
+        setLoading(false);
+        setDataProduct(response.data);
+        setError(null);
+        document.scrollTop = 0;
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if(count > 1) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="product section__padding">
-      <div className="product__image">
-        <img
-          src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-          alt="Clothes"
-        />
-      </div>
-      <div className="product__content">
-        <div className="product__content-attr">
-          <h5 className="product__content-attr_category">men's clothing</h5>
-          <h1 className="product__content-attr_name">
-            Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-          </h1>
-          <p className="product__content-attr_description">
-            Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday
-          </p>
-          <h4 className="product__content-attr_price">$128.9</h4>
-        </div>
-        <div className="product__content-action">
-          <div className="product__content-action_count">
-            <button className="product__content-action_count-decre">-</button>
-            <h4>1</h4>
-            <button className="product__content-action_count-incre">+</button>
+    <div className="section__padding">
+      {loading && (
+        <div className="loading__bar section__margin">Loading...</div>
+      )}
+      {dataProduct && !error && !loading && (
+        <div className="product section__padding">
+          <div className="product__image">
+            <img src={dataProduct.image} alt="Clothes" />
           </div>
-          <button className="btn-main product__content-action_addtocart">
-            ADD TO CART
-          </button>
+          <div className="product__content">
+            <div className="product__content-attr">
+              <h5 className="product__content-attr_category">
+                {dataProduct.category}
+              </h5>
+              <h1 className="product__content-attr_name">
+                {dataProduct.title}
+              </h1>
+              <p className="product__content-attr_description">
+                {dataProduct.description}
+              </p>
+              <h4 className="product__content-attr_price">
+                $ {dataProduct.price}
+              </h4>
+            </div>
+            <div className="product__content-action">
+              <div className="product__content-action_count">
+                <button onClick={decrement} className="product__content-action_count-decre">
+                  -
+                </button>
+                <h4>{ count }</h4>
+                <button onClick={increment} className="product__content-action_count-incre">
+                  +
+                </button>
+              </div>
+              <button className="btn-main product__content-action_addtocart">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      {error && <p className="loading__bar">{error}</p>}
     </div>
   );
 };
