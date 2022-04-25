9e908b4ea96a87b0d8f0cb7aa3e79e334ddd4f9d
juanangelaalma <juanlah77@gmail.com>
2022-04-25 08:43:54 +0700

    add footer link

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,6 +6,7 @@ import loveLogo from "../../assets/logo/love.png";
 import { FooterItem } from "../../components";
 
 import "./footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -17,31 +18,37 @@ const Footer = () => {
       <div className="footer__content">
         <FooterItem title="Composition of website">
           <div className="footer__content-logo">
-            <img src={reactLogo} alt="" />
-            <img src={fakeStoreLogo} alt="" />
-            <img src={loveLogo} alt="" />
+            <a target="_blank" href="https://reactjs.org">
+              <img src={reactLogo} alt="React JS logo" />
+            </a>
+            <a target="_blank" href="https://fakestoreapi.com" >
+              <img src={fakeStoreLogo} alt="Fake Store logo" />
+            </a>
+            <a target="_blank" href="https://github.com/juanangelaalma">
+              <img src={loveLogo} alt="Love icon" />
+            </a>
           </div>
         </FooterItem>
         <FooterItem title="MYSHOP">
           <ul>
             <li className="footer__content-item_list">
-              <a href="">Produk</a>
+              <Link to="/products">Produk</Link>
             </li>
             <li className="footer__content-item_list">
-              <a href="">About</a>
+              <Link to="/about">About</Link>
             </li>
           </ul>
         </FooterItem>
         <FooterItem title="Contact">
           <ul>
             <li className="footer__content-item_list">
-              <a href="">Instagram</a>
+              <a target="__blank" href="https://www.instagram.com/juanalmaa/">Instagram</a>
             </li>
             <li className="footer__content-item_list">
-              <a href="">Linkedin</a>
+              <a target="__blank" href="https://www.linkedin.com/in/juanangelaalma/">Linkedin</a>
             </li>
             <li className="footer__content-item_list">
-              <a href="">Whatsapp</a>
+              <a href="mailto:juanangelaalma@gmail.com">Email</a>
             </li>
           </ul>
         </FooterItem>
