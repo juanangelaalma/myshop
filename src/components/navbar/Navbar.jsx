5dac2bd95f0769a055adc27b1482085b58bf9dcb
juanangelaalma <juanlah77@gmail.com>
2022-04-25 08:45:24 +0700

    add brand link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,56 +1,76 @@
-import React, { useRef, useState } from 'react'
-import { RiCloseLine, RiMenu3Line } from 'react-icons/ri'
-import Search from '../input-search/Search'
+import React, { useRef, useState } from "react";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import Search from "../input-search/Search";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import "./navbar.css"
-import { useSelector } from 'react-redux'
+import "./navbar.css";
+import { useSelector } from "react-redux";
 
 const Menu = () => {
-  const menu = useSelector(state => state.menu)
+  const menu = useSelector((state) => state.menu);
 
-  const {active} = menu
+  const { active } = menu;
   return (
     <>
-      <p className={active === "home" ? "active" : ""}> <Link to="/">Home</Link> </p>
-      <p className={active === "products" ? "active" : ""}> <Link to="/products">Products</Link> </p>
-      <p className={active === "about" ? "active" : ""}> <Link to="/about">About</Link> </p>
+      <p className={active === "home" ? "active" : ""}>
+        {" "}
+        <Link to="/">Home</Link>{" "}
+      </p>
+      <p className={active === "products" ? "active" : ""}>
+        {" "}
+        <Link to="/products">Products</Link>{" "}
+      </p>
+      <p className={active === "about" ? "active" : ""}>
+        {" "}
+        <Link to="/about">About</Link>{" "}
+      </p>
       <Search />
     </>
-  )
-}
+  );
+};
 
 const Navbar = () => {
-  const menu = useRef()
-  const [toggleMenu, setToggleMenu] = useState(false)
+  const menu = useRef();
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className='header__navbar'>
+    <div className="header__navbar">
       <div className="header__navbar-links">
-        <div className='header__navbar-links_logo'>
-          <h1>MYSHOP</h1>
+        <div className="header__navbar-links_logo">
+          <Link to="/">
+            <h1>MYSHOP</h1>
+          </Link>
         </div>
         <div className="header__navbar-links_container">
           <Menu />
         </div>
       </div>
       <div className="header__navbar-menu">
-        { 
-          toggleMenu
-          ? <RiCloseLine size={27} color="#000" onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line size={27} color="#000" onClick={() => setToggleMenu(true)} />
-        }
-        {
-          toggleMenu && (
-            <div ref={menu} className="header__navbar-menu_container scale-up-center">
-              <Menu />
-            </div>
-          )
-        }
+        {toggleMenu ? (
+          <RiCloseLine
+            size={27}
+            color="#000"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            size={27}
+            color="#000"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div
+            ref={menu}
+            className="header__navbar-menu_container scale-up-center"
+          >
+            <Menu />
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
