a09e9996ab10ea855d4d606833b3502089d0d5db
juanangelaalma <juanlah77@gmail.com>
2022-06-01 11:02:31 +0700

    add middleware auth

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import "./App.css"
 import { About, Catalog, Home, Product } from './pages'
 import { Catalog as CatalogContainer } from './containers'
 import Login from './pages/login/Login'
+import Middleware from './components/auth/Middleware'
 
 const App = () => {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path="products" element={<Catalog />}>
+          <Route index element={<Middleware> <Home /> </Middleware>} />
+          <Route path="home" element={<Middleware> <Home /> </Middleware>} />
+          <Route path='about' element={<Middleware> <About /> </Middleware>} />
+          <Route path="products" element={<Middleware> <Catalog /> </Middleware>}>
             <Route index element={<CatalogContainer buttomMore="false" all categories />} />
             <Route path=":idProduct" element={<Product />} />
             <Route path="category/:category" element={<CatalogContainer buttomMore="false" all categories />} />
